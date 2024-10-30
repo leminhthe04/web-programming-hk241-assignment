f242d34c45084b2b28eae1e94805b27178ba5001
leminhthe04 <the.leminh04@hcmut.edu.vn>
2024-10-30 12:17:01 +0700

    update some some :)))

---
+++ api/test.php
@@ -0,0 +1,18 @@
+<?php
+
+
+
+$conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully";
+
+$conn->close();
+
+
+
+
+?>
