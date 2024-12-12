7541077034dd80d2358cdd115f70903007c6f6ce
viLam11 <ngoc.huynhcs2212257@hcmut.edu.com>
2024-12-13 05:01:09 +0700

    fix usermanage

--- Frontend/src/pages/UsersManagement.jsx
+++ Frontend/src/pages/UsersManagement.jsx
@@ -1,42 +1,45 @@
+
 import { useState, useEffect } from "react";
 import Header from "../components/Header";
-import Footer from "../components/Footer";
 import axios from "axios";
-import Pagination from "../components/Pagination";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faMagnifyingGlass } from "@fortawesome/free-solid-svg-icons";
+import { useNavigate } from "react-router-dom";
 
 export default function UsersManagement() {
+    const navigate = useNavigate();
     const [totalUser, setTotalUser] = useState(null);
-    const [userData, setUserData] = useState({});
+    const [sellerList, setSellerList] = useState([]);
     const [page, setTotalPage] = useState(null);
     const [currentPage, setCurrentPage] = useState(0);
 
     useEffect(() => {
-        axios.get("")
+
+            axios.post("http://localhost/Assignment/Backend/api/user/fetch/0/10")
             .then((response) => {
-                const userData = response.data.data;
-                console.log(userData.data);
-                setUserData(userData.data); 
-                setTotalPage(userData.page_count);
-                console.log(page, currentPage)
+                console.log(response.data.data);
+                const result = response.data.data;
+                // console.log(result.data.data);
+                setSellerList(result);
+                // setTotalUser(response.data[totalUser]);
+                // setTotalPage(response.data["totalPage"]);
+                // console.log(page, currentPage)
+                console.log(result);
             })
             .catch((err) => {
-                alert(err.msg);
+                // alert(err.msg);
             });
     }, [])
 
+    console.log("CHECK SELLER LIST: " , sellerList);
 
     function handlePageClick(pageNum) {
         const index = Number(pageNum);
-        const offset = index * 10;
-        axios.get(`http://localhost/Assignment/Backend/api/user/fetch/${offset}/10`,)
+        axios.get(`http://localhost:8000/api/user/users?page=${index}&limit=10`,)
             .then((response) => {
-
+                console.log(response);
                 setCurrentPage(pageNum)
-                const userData = response.data.data;
-                console.log(userData.data);
-                setUserData(userData.data); 
+                const users = response.data.data;
+                // console.log(JSON.stringify(products));
+                setUserData(users);
             })
             .catch((error) => {
                 if (error.response) {
@@ -54,129 +57,80 @@ export default function UsersManagement() {
             <main className="flex-grow bg-purple-2 w-full">
                 <div className="m-4 pl-20">
                     <span className="text-gray-600">Shop / </span>
-                    <span className="font-medium">Customer Management</span>
-                </div>
-
-                <div className=" h-14 flex justify-center items-center w-full">
-                    <div className="w-80 flex flex-row justify-center items-center">
-                        <div className="w-1/4 p-0">
-                            <img src="/users-gr.png" alt="" className=" object-contain" width={70} />
-                        </div>
-                        <div className="col-2 p-0 w-2/3 text-xs">
-                            <div className="w-ful text-gray-500">Tổng người dùng</div>
-                            <div className="w-ful text-xl font-bold">{totalUser}</div>
-                            <div className="w-ful flex flex-row items-center">
-                                <span>
-                                    <img src="/arrow-up.png" alt="" width={20} />
-                                </span>
-                                <span className="mr-1 text-green-600 font-semibold">16% </span>
-                                <span>trong tháng</span>
-                            </div>
-                        </div>
-                    </div>
+                    <span className="font-medium">User Management</span>
                 </div>
 
                 <div className="bg-white w-10/12 min-h-96 m-auto mt-10 rounded-3xl">
-                    <div className="font-bold text-lg px-6 pt-6">Tất cả khách hàng</div>
-                    <div className="flex justify-end ">
-                        <div className="w-5/12 mr-10 flex flex-row">
-                            <div className="search w-1/2 ">
-                                <div className="bg-blue-50 rounded-xl h-full flex items-center my-auto space-x-2 pl-2 border border-black object-cover">
-                                    <div className="w-4">
-                                        <FontAwesomeIcon icon={faMagnifyingGlass} color="black" />
-                                    </div>
-                                    <div className="w-4/5 h-full bg-blue-50">
-                                        <input type="text" placeholder="Tìm kiếm" className="h-full bg-blue-50 border-none outline-none" />
-                                    </div>
-                                </div>
-                            </div>
-                            <div className="sort w-48 mx-auto">
-                                <div className="bg-blue-50 rounded-xl h-full flex items-center my-auto space-x-0 pl-2 object-fill border border-black">
-                                    <span>Sắp xếp theo: </span>
-                                    <span>
-                                        <select name="sort-user" id="sort" className="bg-blue-50 font-semibold rounded-2xl border-none outline-none">
-                                            <option value="newest">Mới nhất</option>
-                                            {/* <option value="expense">Chi tiêu</option> */}
-                                        </select>
-                                    </span>
-                                </div>
-                            </div>
+                    <div className="font-bold text-lg px-6 pt-6">Tất cả người bán</div>
 
 
-
-                        </div>
-                    </div>
-
-                    <div className="w-11/12 m-auto mt-6" style={{height:"540px"}} >
+                    <div className="w-10/12 m-auto mt-6" style={{ height: "540px" }} >
                         <table className="border-none w-full" >
                             <thead>
-                                <tr className="w-full text-gray-500 h-6 text-base">
-                                    <td className="w-10 block ">STT</td>
-                                    <td className="w-3/12 ">Tên khách hàng</td>
-                                    <td className="w-1/6 ">Chi tiêu</td>
+                                <tr className="w-full h-6 font-bold text-base">
+                                    <td className="w-1/6 text-center ">STT</td>
+                                    <td className="w-1/6 ">Tên người bán</td>
+                                    <td className="w-1/6 ">Số điện thoại</td>
                                     <td className="w-1/6 ">Email</td>
-                                    <td className="w-2/6 text-center">Địa chỉ</td>
-                                    <td className="w-1/12 text-center">Thông tin</td>
-                                    
+                                    <td className="w-1/6">Thông tin</td>
+
                                 </tr>
                                 <tr className="h-2 border-b border-zinc-300 my-10"></tr>
 
                             </thead>
                             <tbody>
-                                
-                                {userData.length > 0 ?  userData.map((user, index) => {
-                                    return( 
-                                        <>
-                                            <tr className="h-3" ></tr>
-                                            <tr className="w-full h-6">
-                                                <td className="w-10 block ">{index + 1}</td>
-                                                <td className="w-3/12 ">{user.lname + " " + user.fname}</td>
-                                                <td className="w-1/6 ">{user.total_payment}</td>
-                                                <td className="w-1/6 ">{user.email}</td>
-                                                <td className="w-2/6"></td>
-                                                <td className="w-1/12 text-center ">
-                                                    <a href={`/admin/history/${user.uid}`}className=" border-2 border-green-500 px-3 rounded-md bg-green-100 font-semibold text-green-700 py-1">Chi tiết</a>
-                                                </td>
-                                            </tr>
-                                            <tr className="border-b border-zinc-300 my-10 h-3"></tr>
-                                        </>
-                                        
-                                    )
-                                }) : null}
+
+                                {sellerList.map((seller, index) => (
+                                    <>
+                                    <tr className="h-2"></tr>
+                                    <tr className="w-full h-6  text-base" >
+                                        <td className="w-1/6 text-center ">{index + 1}</td>
+                                        <td className="w-1/6 ">{seller.fname + " " + seller.lname}</td>
+                                        <td className="w-1/6 ">{seller.phone}</td>
+                                        <td className="w-1/6 ">{seller.email}</td>
+                                        <td className="w-1/6">
+                                            <button className="bg-green-600 text-white px-4 py-1 rounded-lg"
+                                                onClick={() => {navigate(`/seller-detail/${seller.id}`)}}
+                                            >Chi tiết</button>    
+                                        </td>
+
+                                    </tr>
+                                    </>
+                                   
+                                ))}
                             </tbody>
                         </table>
 
-                        
+
                     </div>
-                    <div className="h-16 flex flex-end justify-end items-end">    
+                    <div className="h-16 flex flex-end justify-end items-end">
                         <div className="mb-4 mr-10">
-                        <div className="flex justify-end mr-20">
-                        {/* <Pagination /> */}
-                            {Array.from({ length: page }, (_, i) => (
-                                <button
-                                    key={i}
-                                    onClick={() => handlePageClick(i)} // Pass the page number to the handler
-                                    className={`px-3 py-1 mx-1 hover:bg-blue-300 ${currentPage === i ? "bg-blue-500 text-white" : "bg-gray-200"
-                                        } rounded`}
-                                >
-                                    {i + 1}
-                                </button>
-                            ))}
-                        </div>
+                            <div className="flex justify-end mr-20">
+                                {/* <Pagination /> */}
+                                {Array.from({ length: page }, (_, i) => (
+                                    <button
+                                        key={i}
+                                        onClick={() => handlePageClick(i)} // Pass the page number to the handler
+                                        className={`px-3 py-1 mx-1 hover:bg-blue-300 ${currentPage === i ? "bg-blue-500 text-white" : "bg-gray-200"
+                                            } rounded`}
+                                    >
+                                        {i + 1}
+                                    </button>
+                                ))}
+                            </div>
                         </div>
                     </div>
                 </div>
 
 
 
-            
+
                 <div className="h-20">
-                    
+
                 </div>
             </main >
 
             <div className="h-10"></div>
-            <Footer />
         </div >
     )
 
